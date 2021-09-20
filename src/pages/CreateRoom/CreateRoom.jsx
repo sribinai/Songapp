e72ba8a85ca2b990d6ef82d3952df55194da6c6c
unknown <godsonemmanuel07@gmail.com>
2021-09-20 20:11:48 +0530

    Changes in createRoom page

--- src/pages/CreateRoom/CreateRoom.jsx
+++ src/pages/CreateRoom/CreateRoom.jsx
@@ -7,7 +7,11 @@ const CreateRoom = () => {
   return (
     <div className='main-container'>
       <MainHeaderDiv title='Join Room' />
-      <div className='join-room-div'>hello</div>
+      <div className='create-room-div'>
+        <div className='create-room-inputs'>
+          <div className='create-column1'></div>
+        </div>
+      </div>
     </div>
   );
 };
