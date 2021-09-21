50a5157728946250752bf9eec6e089816df5dd28
sribinai <ammumatambi@gmail.com>
2021-09-21 12:06:22 +0530

    CreateRoom changes

--- src/pages/CreateRoom/CreateRoom.jsx
+++ src/pages/CreateRoom/CreateRoom.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainHeaderDiv from "../../components/MainHeaderDiv/MainHeaderDiv";
+import { Form, Button } from "react-bootstrap";
 
 import "./create-room.styles.css";
 
@@ -9,7 +10,47 @@ const CreateRoom = () => {
       <MainHeaderDiv title='Join Room' />
       <div className='create-room-div'>
         <div className='create-room-inputs'>
-          <div className='create-column1'></div>
+          <div className='create-column1'>
+          {
+          
+          <div className="buttons">
+               <h1>Create Room</h1>
+              <Button variant="primary" type="submit">ROOM ID: THXQL ❏</Button>
+             
+            
+          </div>
+        }
+          {
+          
+          <div classname="inputs">
+             <label>Room Name:</label>
+               <input type="text" name="save" className="text1" />
+               <Button variant="primary" type="submit" className="save">SAVE</Button>
+             
+          </div>
+        }
+           
+          { 
+
+         <div className="buttons2">
+           <div className="pass">
+           <label> Passcode:  </label>
+           </div>
+             <input type="text" name="copy" className="text2" />
+             <Button variant="primary" type="submit" className="copy">❏</Button>
+           
+          </div>
+          }
+          
+          { 
+
+          <div className="buttons">
+          <Button variant="primary" type="submit" className="btn1">GENERATE PASSCODE</Button>
+          
+          </div>
+          }
+           
+          </div>
         </div>
       </div>
     </div>
