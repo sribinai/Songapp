e00a442a17a35f222c553978764e5e27bdff0795
MdSarmad <28mdsarmad99@gmail.com>
2021-09-22 01:41:35 +0530

    changed styles and made some responsiveness

--- src/components/Form/Textarea.jsx
+++ src/components/Form/Textarea.jsx
@@ -4,7 +4,8 @@ const Textarea = ({ title }) => {
   return (
     <div className='input-div'>
       <h3 className='title'>{title}</h3>
-      <textarea name='textarea'></textarea>
+      <textarea name="textarea" id="ta" cols="30" rows="8"></textarea>
+      {/* <textarea name='textarea'></textarea> */}
     </div>
   );
 };
