49f9074deb30afef45bd4599548111f0a943af69
unknown <godsonemmanuel07@gmail.com>
2021-11-15 22:06:56 +0530

    added data from backend for protecting the routes in react

--- client/src/functionalities/ProtectedRoutes.jsx
+++ client/src/functionalities/ProtectedRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect, Route, useHistory } from "react-router-dom";
 import axios from "axios";
 import { DATA_URL } from "../index";
@@ -8,58 +8,56 @@ import { useCookies } from "react-cookie";
 function ProtectedRoute({ component: Component, ...restOfProps }) {
   const history = useHistory();
   const [cookies, setCookie] = useCookies(["playlist_token"]);
-  const isAuthenticated =
-    localStorage.getItem("isAuthenticated") === "true" ? true : false;
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   // On mount check if the token data exists in DB
-  // useEffect(() => {
-  //   // console.log("on mount");
-  //   checkValidToken();
-  // }, []);
+  useEffect(() => {
+    if (!userInfo) {
+      checkValidToken();
+    }
+  }, [userInfo]);
   // // API call to check if the token available is valid
-  // const checkValidToken = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `${DATA_URL}/playlist/api/user/get-data`
-  //     );
-  //     console.log(response);
-  //   } catch (err) {
-  //     console.log(err);
-  //     console.log(err.response);
-  //     // if (err.response.data.message) {
-  //     //   // Redirect to login page
-  //     //   history.push({
-  //     //     pathname: "/login-signup",
-  //     //     search: "?user=unathorized",
-  //     //     state: { message: err.response.data.message },
-  //     //   });
-  //     // } else {
-  //     //   history.push({
-  //     //     pathname: "/login-signup",
-  //     //     search: "?access=error",
-  //     //     state: { message: "Unexpected error." },
-  //     //   });
-  //     // }
-  //   }
-  // };
-
-  return (
-    <Route
-      {...restOfProps}
-      render={(props) =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login-signup",
-              state: { signup: false },
-            }}
-          />
-        )
+  const checkValidToken = async () => {
+    try {
+      const response = await axios.get(
+        `${DATA_URL}/playlist/api/user/get-data`,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(response);
+      setUserInfo(response.data);
+      setIsLoaded(true);
+    } catch (err) {
+      console.log(err);
+      if (err.response) {
+        console.log(err.response);
       }
-    />
-  );
+      setIsLoaded(true);
+    }
+  };
+
+  if (!isLoaded) {
+    return <p>Loading....</p>;
+  } else if (isLoaded === true && userInfo === null) {
+    return (
+      <Redirect
+        to={{
+          pathname: "/login-signup",
+          search: "?user=unauthorized",
+          state: { signup: false },
+        }}
+      />
+    );
+  } else {
+    return (
+      <Route
+        {...restOfProps}
+        render={(props) => <Component {...props} {...userInfo} />}
+      />
+    );
+  }
 }
 
 export default ProtectedRoute;
